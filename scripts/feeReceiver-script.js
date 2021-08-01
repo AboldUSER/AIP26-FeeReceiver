96d402e47b77ffa0c5255d65b12e41e3c076bbd8
bold <2985057+AboldUSER@users.noreply.github.com>
2021-08-01 16:53:46 -0700

    Update deploy script

--- scripts/feeReceiver-script.js
+++ scripts/feeReceiver-script.js
@@ -8,6 +8,13 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log(deployer.address);
 
+  const swapToToken = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; // placeholder token
+  const uniRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"; // placeholder address
+  const triggerFee = 1;
+  const payees = ["0x7296333e1615721f4Bd9Df1a3070537484A50CF8"]; // placeholder address
+  const shares = [10];
+
+
   // Deploy the contract
   const FeeReceiver = await hre.ethers.getContractFactory("FeeReceiver");
   const feeReceiver = await FeeReceiver.deploy(
